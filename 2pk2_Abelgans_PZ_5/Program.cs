81507c0bca032fce4232f2ce0c3b2a55caa65945
abelgans.kv1678 <abelgans.kv1678@M409C-5L>
2022-09-26 11:39:53 +0500

    Добавьте файлы проекта.

---
+++ 2pk2_Abelgans_PZ_5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2pk2_Abelgans_PZ_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 90;
+            for (; a <= b; a += 5)
+            {
+                Console.WriteLine(a.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
