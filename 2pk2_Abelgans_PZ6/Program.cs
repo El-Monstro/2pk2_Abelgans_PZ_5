bd145d977075f65c60ecbeab71e70f0e60c9b623
Кирилл Абельганс <kabelgans.2017@gmail.com>
2022-09-29 11:08:21 +0500

    \

--- 2pk2_Abelgans_PZ6/Program.cs
+++ 2pk2_Abelgans_PZ6/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+
+            int i = 1;
+            int y = 20;
+            double x = 10.0;
+            double r;
+            do
+            {
+                i++;
+                r = x * 0.1;
+                x += r;
+            }
+            while (x <= y);
+            Console.WriteLine(i);
         }
     }
 }
